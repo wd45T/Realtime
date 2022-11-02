c874f8b43f92bb430573218f1756f3ab969efbc0
Artem Shpilevoi iLogos <artem.shpilevoi@ilogos.biz>
2022-11-02 19:37:13 +0300

    added solution

---
+++ Realtime.Chat.API/Controllers/ChatController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realtime.Chat.API.Controllers
+{
+    public class ChatController : ControllerBase
+    {
+
+        [HttpGet("LongPolling")]
+        public async Task<IActionResult> LongPollingAsync()
+        {
+            await Task.CompletedTask;
+
+            return Ok("Hello, World!");
+        }
+    }
+}
