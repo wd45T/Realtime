39cb8d3aef6aed66247bb9b3404f03275af0e177
Artem Shpilevoi iLogos <artem.shpilevoi@ilogos.biz>
2022-11-03 20:30:03 +0300

    fix di

--- Realtime.Chat.API/Program.cs
+++ Realtime.Chat.API/Program.cs
@@ -1,5 +1,6 @@
 using Realtime.Chat.Service.Implementations;
 using Realtime.Chat.Service.Interfaces;
+using Realtime.Engine.Repositories.Implementations;
 using Realtime.Engine.Repositories.Interfaces;
 using Realtime.Engine.Services;
 using Realtime.Engine.Services.Implementations;
@@ -15,7 +16,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IChatService, ChatService>();
 builder.Services.AddScoped<IRealTimeEventService, LongPollingEventService>();
-builder.Services.AddScoped<ILongPollingRepository, LongPollingtByRedisRepository>();
+builder.Services.AddScoped<ILongPollingRepository, LongPollingByRedisRepository>();
 
 var redisUrl = builder.Configuration.GetValue<string>("RedisUrl");
 
