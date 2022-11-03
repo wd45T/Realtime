a7a11dbdfb7f9a0408150229ad0d1daa0ffe5190
Artem Shpilevoi iLogos <artem.shpilevoi@ilogos.biz>
2022-11-03 22:12:04 +0300

    implemented client send message

--- Realtime.Chat.Client/Program.cs
+++ Realtime.Chat.Client/Program.cs
@@ -3,6 +3,8 @@ using Realtime.Chat.Common.TransportLayer;
 using Realtime.Chat.Common.TransportLayer.Commands.Request;
 using System.Text;
 
+var serverUrl = "http://localhost:5289";
+
 var clientSessionId = Guid.NewGuid();
 
 var chatId = Guid.Parse("dcada075-07a7-4372-851c-8591aaa440f2");
@@ -18,15 +20,49 @@ using (var httpClient = new HttpClient())
         ChatId = chatId
     };
 
-    var subscribeToChatResponse = await httpClient.PostAsync("http://localhost:5289/SubscribeToChat", GetStringContent(subscribeToChatRequest));
+    var subscribeToChatResponse = await httpClient.PostAsync($"{serverUrl}/SubscribeToChat", GetStringContent(subscribeToChatRequest));
 
     if (subscribeToChatResponse.IsSuccessStatusCode)
     {
         Console.WriteLine($"Successfully connected to the chat: {chatId}");
 
+        _ = ReceiveMessagesAsync(clientSessionId, serverUrl);
+
         while (true)
         {
-            var receiveMessagesResponse = await httpClient.GetAsync("http://localhost:5289/ReceiveMessages");
+            var message = Console.ReadLine();
+
+            ClearCurrentConsoleLine();
+
+            var sendMessageRequest = new SendMessageRequest
+            {
+                ChatId = chatId,
+                Message = message
+            };
+
+            await httpClient.PostAsync($"{serverUrl}/SendMessage", GetStringContent(sendMessageRequest));
+        }
+    }
+}
+
+static void ClearCurrentConsoleLine()
+{
+    Console.SetCursorPosition(0, Console.CursorTop - 1);
+    int currentLineCursor = Console.CursorTop;
+    Console.SetCursorPosition(0, Console.CursorTop);
+    Console.WriteLine(new string(' ', Console.WindowWidth));
+    Console.SetCursorPosition(0, currentLineCursor);
+}
+
+static async Task ReceiveMessagesAsync(Guid clientSessionId, string serverUrl)
+{
+    using (var httpClient = new HttpClient())
+    {
+        httpClient.DefaultRequestHeaders.Add(Headers.ClientSessionId, clientSessionId.ToString());
+
+        while (true)
+        {
+            var receiveMessagesResponse = await httpClient.GetAsync($"{serverUrl}/ReceiveMessages");
 
             var messages = await GetResultAsync<List<string>>(receiveMessagesResponse);
 
